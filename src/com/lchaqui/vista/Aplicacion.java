f75d85034f1f4ff96f83316702bc06d852ac9a6c
LuisChaqui <luigi92pm@gmail.com>
2018-10-24 11:48:56 -0500

    Este es una primera version del proyecto colaborativo III

---
+++ src/com/lchaqui/vista/Aplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lchaqui.vista;
+
+/**
+ *
+ * @author LAB5-PC8-USUARIO
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
